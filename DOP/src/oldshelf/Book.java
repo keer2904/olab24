a3e98ec3a19cdf653bb6921aa62350c8002169a5
keer2904 <118612242+keer2904@users.noreply.github.com>
2024-10-27 14:43:54 +0530

    Update Book.java

--- DOP/src/oldshelf/Book.java
+++ DOP/src/oldshelf/Book.java
@@ -1,8 +1,20 @@
 package oldshelf;
-
-public abstract class Book {
-
-	public Book() {
-		// TODO: Finish Code here if anything to be done?
+import newshelf.IBook;
+public abstract class Book 
+{
+	public Book(String title, String author) // TODO: Finish Code here if anything to be done?
+	{
+		this.title=title;
+		this.author=author;
+	}
+	@Override
+	public String getTitle()
+	{
+		return title;
+	}
+	@Override
+	public String getAuthor()
+	{
+		return author;
 	}
 }
