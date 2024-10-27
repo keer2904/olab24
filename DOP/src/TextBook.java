139af4469c2f25df57c2f33f9708f11ebcdad0ac
keer2904 <118612242+keer2904@users.noreply.github.com>
2024-10-27 16:24:01 +0530

    Update TextBook.java

--- DOP/src/TextBook.java
+++ DOP/src/TextBook.java
@@ -1,5 +1,15 @@
-
 // TODO: Complete this with record components
-public record TextBook() {
-
+package newshelf;
+import oldshelf.TextBook as OldTextBook;
+public record TextBook(String subject) implements IBook 
+{
+    public boolean isEquivalentToOldTextBook(OldTextBook oldTextBook) 
+    {
+        return oldTextBook != null && this.subject.equals(oldTextBook.getSubject());
+    }
+    @Override
+    public String toString() 
+    {
+        return "NewShelf TextBook [Subject: " + subject + "]";
+    }
 }
